d1a788a841cc1c9f752b2fb331a5949fb34b09d5
hsjo12 <hsjo12@naver.com>
2024-12-03 19:00:16 +0900

    updatE

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import {
   cookieStorage,
   createConfig,
 } from "@account-kit/react";
-import { alchemy, sepolia } from "@account-kit/infra";
+import { alchemy, sepolia, baseSepolia } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 
 const uiConfig: AlchemyAccountsUIConfig = {
@@ -14,7 +14,6 @@ const uiConfig: AlchemyAccountsUIConfig = {
       [
         { type: "passkey" },
         { type: "social", authProviderId: "google", mode: "popup" },
-        { type: "social", authProviderId: "facebook", mode: "popup" },
       ],
       [
         {
@@ -29,8 +28,9 @@ const uiConfig: AlchemyAccountsUIConfig = {
 
 export const config = createConfig(
   {
-    transport: alchemy({ apiKey: "ALCHEMY_API_KEY" }), // TODO: add your Alchemy API key - https://dashboard.alchemy.com/accounts
-    chain: sepolia,
+    transport: alchemy({ apiKey: "N2t7mhBdiWKKc7SMTYVN_T-d9R6qBCkG" }), // TODO: add your Alchemy API key - https://dashboard.alchemy.com/accounts
+    chain: baseSepolia,
+    policyId: "b45126da-49c2-44d7-a251-b33f38d25a9a",
     ssr: true, // more about ssr: https://accountkit.alchemy.com/react/ssr
     storage: cookieStorage, // more about persisting state with cookies: https://accountkit.alchemy.com/react/ssr#persisting-the-account-state
     enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
